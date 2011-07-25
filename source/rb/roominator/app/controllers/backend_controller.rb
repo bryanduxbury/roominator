d1123c49196ee88ccd2fb65a8aafd08dd3ac7f8c
Oliver Sisson <oliversisson@gmail.com>
2011-07-25 15:22:18 -0700

    got index.haml working

---
+++ source/rb/roominator/app/controllers/backend_controller.rb
@@ -0,0 +1,14 @@
+class BackendController < ApplicationController
+  # gives time remaining in current meeting and time left till next meeting
+  def give_recent_meeting_info
+    
+  end
+  
+  def book_meeting
+    
+  end
+  
+  def what_is_my_name
+    
+  end
+end
