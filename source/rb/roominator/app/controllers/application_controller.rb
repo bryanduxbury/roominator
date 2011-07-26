fd091bb056130bfe9703bd1bd1eaaef2b551f4a6
Gabriel Isman <gabe.isman@gmail.com>
2011-07-25 19:29:11 -0700

    add_event

--- source/rb/roominator/app/controllers/application_controller.rb
+++ source/rb/roominator/app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  require 'yaml'
   protect_from_forgery
+
+  private
+
+  def authenticate_to_gcal
+    if !@service
+      auth_data = YAML::load(File.open("config/authentication.yml"))
+      begin
+        @service = GCal4Ruby::Service.new(auth_data[:email], auth_data[:password])
+      rescue AuthenticationFailedError
+        @service = nil #this oughta be nicer TODO
+      end
+    end
+  end
+
 end
--- source/rb/roominator/app/controllers/room_controller.rb
+++ source/rb/roominator/app/controllers/room_controller.rb
@@ -1,5 +1,7 @@
 class RoomController < ApplicationController
 
+  before_filter :authenticate_to_gcal
+
   def index
     @rooms = Room.all
   end
@@ -9,11 +11,20 @@ class RoomController < ApplicationController
   end
 
   def add_event
-
+    room = Room.find_by_room_id(params[:room])
+    calendar = Calendar.find(@service, {:id => room.calendar_id})
+    event = Event.new(@service, {:calendar => calendar,
+                                 :title => "Roomination",
+                                 :start => Time.now,
+                                 :end => Time.now + 15.minutes,
+                                 :where => room.room_name})
+    event.save
   end
 
   def extend_event
-
+    room = Room.find_by_room_id(params[:room])
+    calendar = Calendar.find(@service, {:id => room.calendar_id})
+    events = Calendar.find(@service, {'start-max' => Time.now.utc.xmlschema, 'end-min' => Time.now.utc.xmlschema})
   end
 
   def room_free?
