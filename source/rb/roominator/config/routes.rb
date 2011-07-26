d67ae049696b7d81e05175a1ece4b65f1e3f4a9e
Oliver Sisson <oliversisson@gmail.com>
2011-07-25 18:50:07 -0700

    added unique tag

--- source/rb/roominator/config/routes.rb
+++ source/rb/roominator/config/routes.rb
@@ -1,12 +1,9 @@
 Roominator::Application.routes.draw do
   root :to => "home#index"
   
-  #match "/backend/give_recent_meeting_info" => "backend#give_recent_meeting_info"
-  resource :backend
-  
   # See how all your routes lay out with "rake routes"
   
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
--- source/rb/roominator/db/migrate/20110726011558_setup_room_db.rb
+++ source/rb/roominator/db/migrate/20110726011558_setup_room_db.rb
@@ -1,11 +1,9 @@
 class SetupRoomDb < ActiveRecord::Migration
   def self.up
-    add_column :rooms, :name , :string
-    add_column :rooms, :room_number , :int
+    add_column :rooms, :room_number, :int, :unique => true
   end
 
   def self.down
-    remove_column :rooms, :name
     remove_column :rooms, :room_number
   end
 end
