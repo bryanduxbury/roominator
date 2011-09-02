cf35880d5fd8a956144ed391b55f87fb47301ee1
bryanduxbury <bryan@rapleaf.com>
2011-09-01 17:49:10 -0700

    gcal daemon now working correctly for a single pass

--- source/rb/roominator/lib/gcal_daemon.rb
+++ source/rb/roominator/lib/gcal_daemon.rb
@@ -5,19 +5,29 @@ class GcalDaemon
   end
 
   def run
-    while true
-      Room.find_all.each do |room|
-        if cal = @cal_service.calendars.find{|c| c.id == room.calendar_id}
-          current_event = cal.events.select{|e| e.start_time < Time.now || e.end_time > Time.now}.first
+    # while true
+      Room.find(:all).each do |room|
+        puts "working on room #{room.room_name}"
+        if cal = @cal_service.calendars.find{|c| CGI.unescape(c.id) == room.calendar_id}
+          puts "found a calendar: #{cal}"
+          current_event = cal.events.select{|e| e.end_time > Time.now}.sort_by{|e| e.start_time}.first
+          # puts current_event.start_time
+          # puts current_event.end_time
+          # puts current_event.title
+          # puts current_event.attendees.inspect
+          # puts current_event.attendees.select{|a| a[:role] == "organizer"}.first[:name]
+          # puts [current_event.methods - Object.methods].sort.inspect
           if current_event
-            room.event_desc = current_event.content
+            room.event_desc = current_event.title
             room.next_reservation_at = current_event.start_time
             room.reservation_duration_secs = current_event.end_time - current_event.start_time
-            room.save
+            room.reserved_by = current_event.attendees.select{|a| a[:role] == "organizer"}.first[:name]
+            room.save!
           end
         end
       end
-    end
+      # sleep 10000
+    # end
   end
 end
 
